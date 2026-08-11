4c505058f31413383390cef0f8c39c9c595c51b6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-10 22:15:41 -0300

    random

---
+++ src/randomteste.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class randomteste {
+    static void main() {
+        Random random = new Random();
+        int x;
+
+        x = random.nextInt(10);
+
+        System.out.println(x);
+    }
+}
