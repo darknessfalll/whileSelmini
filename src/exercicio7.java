ed8bd5cc63a10c82206407328324f45471326c53
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-17 22:44:38 -0300

    continuação exercicio 7

--- src/exercicio7.java
+++ src/exercicio7.java
@@ -5,10 +5,29 @@ public class exercicio7 {
     static void main() {
         Random random = new Random();
         int[] x = new int[10];
+        int cont, grau = 0;
 
         // geração de valores
         for (int i = 0; i < x.length; i++) {
+            x[i] = random.nextInt(1,10);
+            System.out.println(x[i] + "\t");
 
+        }
+
+        // procurando o grau do vetor
+        for (int i = 0; i < x.length; i++) {
+            cont = 0;
+            for (int j = 0; j < x.length; j++) {
+                if (x[i] == x[j]){
+                    cont++;
+                }
+
+            }
+            if (cont > grau){
+                grau = cont;
+            }
+
+            System.out.println(grau);
         }
 
 
